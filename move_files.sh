88d9c9da7d2ce263ac5aa41fbedb95e824a60f4f
Li-Kuang Chen <lkchen@nlplab.cc>
2024-02-07 01:50:51 +0800

    feat: script for semcor data pre-preprocessing

---
+++ move_files.sh
@@ -0,0 +1,16 @@
+unzip GlossBERT_datasets.zip
+
+mkdir data/examples data/gold_keys
+
+mv ./GlossBERT_datasets/Training_Corpora/SemCor/semcor_train_token_cls.csv ./data/examples
+mv ./GlossBERT_datasets/Training_Corpora/SemCor/semcor.gold.key.txt ./data/gold_keys
+
+eval_datasets=("ALL" "semeval2007" "semeval2013" "semeval2015" "senseval2" "senseval3")
+
+# Loop through the array
+for d in "${eval_datasets[@]}"
+    do
+        mv "./GlossBERT_datasets/Evaluation_Datasets/${d}/${d}_test_token_cls.csv" ./data/examples
+        mv "./GlossBERT_datasets/Evaluation_Datasets/${d}/${d}.gold.key.txt" ./data/gold_keys
+    done
+done
