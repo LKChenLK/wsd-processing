5019060ba73eea3035f5b9ff238202df2b20893c
Li-Kuang Chen <lkchen@nlplab.cc>
2024-02-09 23:29:42 +0800

    feat: train/dev/test split function, add cli options

--- gen_cambridge_data.py
+++ gen_cambridge_data.py
@@ -29,7 +29,7 @@ def format_prompt_input(word: str, sent: str, definitions: List) -> Dict:
     into input prompt for WSD model.
     :param word: word to be disambiguated
     :param sent: context sentence of `word`
-    :param definitions: List[Dict]. Output of `get_cambridge_dict_info()`
+    :param definitions: List[Dict]. Output of `get_cambridge_dict_info()` in linggle booster's word_info
     :return: Dict[str]
     """
     def mark_target(sent: str, target: str) -> str:
@@ -100,37 +100,42 @@ def make_data(camb_dir):
                             }
                         )
                         target_sense_num += 1
-            if len(all_senses) > 1:
-                # separate each example sent to be 1 dataset example
-                for sense in all_senses:
-                    for ex in sense['example_sents']:
-                        if not ex:
-                            continue
-                        if ex[-1] not in string.punctuation:
-                            # only include full sentences as examples
-                            continue
-                        formatted_prompt_input = format_prompt_input(lem, ex, all_senses)
-                        if formatted_prompt_input is not None:
-                            examples.append(
-                                {
-                                    **formatted_prompt_input, # input, all definitions
-                                    "sent": ex,
-                                    "lemma": lem,
-                                    "target_sense_num": sense['target_sense_num'], # answer
-                                    "data-en_def": sense["data-en_def"], 
-                                }
-                            )
-                        else:
-                            non_examples.append(sense)
-            else:
-                non_examples.append(lem)
+
+            # separate each example sent to be 1 dataset example
+            for sense in all_senses:
+                for ex in sense['example_sents']:
+                    if not ex:
+                        continue
+                    if ex[-1] not in string.punctuation:
+                        # only include full sentences as examples
+                        continue
+                    formatted_prompt_input = format_prompt_input(lem, ex, all_senses)
+                    if formatted_prompt_input is not None:
+                        examples.append(
+                            {
+                                **formatted_prompt_input, # input, all definitions
+                                "sent": ex,
+                                "lemma": lem,
+                                "target_sense_num": sense['target_sense_num'], # answer
+                                "data-en_def": sense["data-en_def"], 
+                            }
+                        )
+                    else:
+                        non_examples.append(sense)
     
     out_path = os.path.join(camb_dir, "camb.prompts.jsonl")
     with jsonlines.open(out_path, "w") as f:
         f.write_all(examples)
     print(f"{len(examples)} examples of the entire {FILE_CAM_DICT} saved to {out_path}; {len(non_examples)} not used!")
 
-def split_dataset(dataset_path):
+def split_dataset(dataset_path, prompt_type):
+
+    def format_out(x, y):
+        out_data = []
+        for x_, y_ in zip(x, y):
+            out_data.append({"text": x_, "label": y_})
+        return out_data
+    
     data = [line for line in jsonlines.open(dataset_path)]
     x_all = [it['input'] for it in data]
     y_all = [it['data-en_def'] for it in data]
@@ -142,16 +147,32 @@ def split_dataset(dataset_path):
         = train_test_split(x_test, y_test, test_size=0.5, random_state=42) # 0.2 * 0.5 = 0.1
     # We end up with a 80, 10, 10 split
 
+    train_out = format_out(x_train, y_train)
+    dev_out = format_out(x_dev, y_dev)
+    test_out = format_out(x_test, y_test)
+
+    with jsonlines.open(os.path.join(dataset_path, f"{prompt_type}_train.jsonl"), "w") as f:
+        f.write_all(train_out)
+
+    with jsonlines.open(os.path.join(dataset_path, f"{prompt_type}_dev.jsonl"), "w") as f:
+        f.write_all(dev_out)
+
+    with jsonlines.open(os.path.join(dataset_path, f"{prompt_type}_test.jsonl"), "w") as f:
+        f.write_all(test_out)
+
+    print(f"Split dataset saved to dataset_path/{prompt_type}_{{train,dev,test}}.jsonl")
+
+
 
 if __name__=="__main__":
     parser = argparse.ArgumentParser()
-    parser.add_argument("--cambrigde_dict_dir", )
-    make_data(sys.argv[1])
-
-    # data = [line for line in jsonlines.open("camb.test.jsonl")]
+    parser.add_argument("--cambridge_dict_dir", default=f"{BASE_PATH}/data/cambridge")
+    parser.add_argument("--prompt_type", choices=['generative', 'multiple_choice'], required=True)
+    parser.add_argument("--split", default=False)
     
-    # random.seed(42)
-    # test = random.sample(data, int(len(data)/10))
-    # with jsonlines.open("camb.test.sampled.jsonl", "w") as f:
-    #     f.write_all(test)
+    args = parser.parse_args()
+    make_data(args.cambridge_dict_dir)
+    if args.split:
+        split_dataset(args.cambridge_dict_dir, args.prompt_type)
+
     
