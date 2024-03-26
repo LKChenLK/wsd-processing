3e413a77ff07dd9e9423408bd29024074848203e
li-kuang chen <lkchen@nlplab.cc>
2024-03-26 23:24:58 +0800

    add: ignore logs

---
+++ train_templates/gpt2.sh
@@ -0,0 +1,30 @@
+#!/bin/sh
+
+WORK_DIR="/home/nlplab/$(whoami)/wsd-processing/"
+
+# activate virtual env
+source ${WORK_DIR}/env/bin/activate
+
+set -ex
+
+MODEL_DIR="${WORK_DIR}/model/gpt2"
+PRETRAINED_MODEL="gpt2"
+CUDA_VISIBLE_DEVICES=0,1
+
+python "${WORK_DIR}/train.py" \
+    --pretrained_model ${PRETRAINED_MODEL} \
+    --dataset_path "${WORK_DIR}/data/cambridge/generative" \
+    --model_dir ${MODEL_DIR} \
+    --prompt_type generative \
+    --train_batch_size 16 \
+    --max_length 100 \
+    --do_eval \
+    --evaluation_strategy epoch \
+    --save_steps 2000 \
+    --learning_rate 0.0001 \
+    --epochs 4 \
+    --weight_decay 0.1 \
+    --warmup_steps 1000 \
+    --lr_scheduler_type cosine
+
+set +ex
