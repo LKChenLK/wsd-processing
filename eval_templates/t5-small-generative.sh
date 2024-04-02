15f98dc90115be8d8755c0273b013b7b7c8e2ac2
Li-Kuang Chen <lkchen@nlplab.cc>
2024-04-02 21:38:40 +0800

    add: evaluation script template

---
+++ eval_templates/t5-small-generative.sh
@@ -0,0 +1,5 @@
+python evaluate_wsd.py \
+    --test_path data/cambridge/generative/test.jsonl \
+    --model_dir model/03mar2024_gen_lr_1e-3_cont \
+    --out_path model/03mar2024_gen_lr_1e-3_cont/test.out.jsonl \
+    --prompt_type generative
